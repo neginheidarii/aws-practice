26769deaaec3647eb09c0633f670428ed01ed48d
negin <neginhd6@gmail.com>
2025-03-22 13:30:08 -0400

    added amplify_outputs file, used iframe to use external resources

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,47 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    return () => subscription.unsubscribe(); // Cleanup subscription on unmount
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Enter todo content:");
+    if (content) {
+      client.models.Todo.create({ content });
+    }
   }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>My Todos</h1>
+      <button onClick={createTodo}>+ New</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
+
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
+          Review the next step of this tutorial.
         </a>
+      </div>
+
+      <div>
+        <h2>External Resource</h2>
+        <iframe
+          src="https://neginheidari.com"
+          width="100%"
+          height="500px"
+          style={{ border: "none" }}
+          title="W3Schools"
+        ></iframe>
       </div>
     </main>
   );
